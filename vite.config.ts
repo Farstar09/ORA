c4e9274e8ba5bf93bce72a566e054b7f4208432d
Farstar09 <71098116+Farstar09@users.noreply.github.com>
2026-01-31 14:17:23 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/ORA/",
   plugins: [react()],
+  base: "/ORA/", // <-- MUST match your repo name exactly
 });
